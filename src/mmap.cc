ea770b03c2a0345ea5066d1787e5e8436a787d05
Jonathan Barronville <jonathan@belairlabs.com>
2017-11-26 12:36:47 -0500

    Wrapped some code.

--- src/mmap.cc
+++ src/mmap.cc
@@ -147,7 +147,8 @@ static void Init(Handle<Object> target) {
   target->Set(Nan::New("MS_SYNC").ToLocalChecked(), Nan::New<Number>(MS_SYNC));
   target->Set(Nan::New("MS_INVALIDATE").ToLocalChecked(), Nan::New<Number>(MS_INVALIDATE));
 
-  target->Set(Nan::New("PAGE_SIZE").ToLocalChecked(), Nan::New<Number>(sysconf(_SC_PAGE_SIZE)));
+  target->Set(Nan::New("PAGE_SIZE").ToLocalChecked(), Nan::New<Number>(
+    sysconf(_SC_PAGE_SIZE)));
 }
 
 
